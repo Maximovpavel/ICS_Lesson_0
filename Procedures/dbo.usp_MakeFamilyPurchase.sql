d71043092de36ee25d52a0cde479feaf65b8cf85
Maximovpavel <maximov_pavel_1998@rambler.ru>
2022-12-13 15:09:04 +0300

    Update dbo.usp_MakeFamilyPurchase.sql

--- Procedures/dbo.usp_MakeFamilyPurchase.sql
+++ Procedures/dbo.usp_MakeFamilyPurchase.sql
@@ -1,12 +1,14 @@
 create or alter procedure dbo.usp_MakeFamilyPurchase
 	@FamilySurName varchar(255)
 as
-begin
+begin try
 	if (select ID from dbo.Family where FamilySurName = @FamilySurName) is null
-	raiserror ('Такой семьи нет', 3, 1)
+	raiserror ('Такой семьи нет', 11, 1)
 
 	update dbo.Family
 	set BudgetValue -= (select sum(Value) from dbo.Basket)
 	where FamilySurName = @FamilySurName
-end
-go
+end try
+begin catch
+	;throw
+end catch
